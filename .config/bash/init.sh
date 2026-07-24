18f0b256d92c629b1b5c638382911477d08f472a
yumekiti <yumekiti120440@gmail.com>
2026-07-24 03:01:21 +0000

    updat4e

--- .config/bash/init.sh
+++ .config/bash/init.sh
@@ -2,7 +2,14 @@ eval "$(zoxide init bash)"
 
 alias ls='eza --icons --group-directories-first'
 alias l='eza --icons --group-directories-first -a'
-alias cat='bat --paging=never --style=plain'
+unalias cat 2>/dev/null || true
+cat() {
+  if [ -t 0 ] && [ -t 1 ] && [ "$#" -gt 0 ]; then
+    bat --paging=never --style=plain "$@"
+  else
+    command cat "$@"
+  fi
+}
 vi() {
   if [ $# -eq 0 ]; then
     nvim .
@@ -10,3 +17,4 @@ vi() {
     nvim "$@"
   fi
 }
+
